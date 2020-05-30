b81d1833f54629ff5613463ebdf2bb5482c5cc78
DDI0720 <rlaskdn3615@gmail.com>
2020-05-30 13:50:52 +0900

    팩토리패턴 시작

---
+++ typescript/factory/factory.ts
@@ -0,0 +1,13 @@
+/* -------생산자 클래스-------- */
+class ShoeStroe {}
+class ShoeFactory {}
+class KRshoeFactory extends ShoeFactory {}
+class FRshoeFactory extends ShoeFactory {}
+
+
+/* -------제품 클래스------- */
+class Shoes {}
+class KR_BlackShoe extends Shoes {}
+class KR_PinkShoe extends Shoes {}
+class FR_BlackShoe extends Shoes {}
+class FR_PinkShoe extends Shoes {}
