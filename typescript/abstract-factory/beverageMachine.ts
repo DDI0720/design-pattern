2bae9f1f2e8d8994bff5e5d75876ac2ea5d2abe4
DDI0720 <rlaskdn3615@gmail.com>
2020-06-06 11:31:59 +0900

    케이크,음료종류

--- typescript/abstract-factory/beverageMachine.ts
+++ typescript/abstract-factory/beverageMachine.ts
@@ -4,12 +4,12 @@ interface BeverageMachine {
 
 export class CoffeeMachine implements BeverageMachine {
     process(): void {
-        console.log('커피 없인 못살아 정말못살아!쿵짝!');
+        console.log('커피');
     }
 }
 
 export class MilkMachine implements BeverageMachine {
     process(): void {
-        console.log('카스테라 우유 찍먹 옴뇸뇸');
+        console.log('우유');
     }
 }
--- typescript/abstract-factory/cakeMachine.ts
+++ typescript/abstract-factory/cakeMachine.ts
@@ -4,12 +4,12 @@ interface CakeMachine {
 
 export class CheeseCakeMachine implements CakeMachine {
     process ():void {
-        console.log('치즈케이크 맛있게 굽고있습니다만?');
+        console.log('치즈케이크');
     }
 }
 
 export class CreamCakeMachine implements CakeMachine {
     process (): void {
-        console.log('생크림 바르고 있습니다만?');
+        console.log('생크림케이크');
     }
 }
