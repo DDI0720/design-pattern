0b91c6513ce1ddc6dabf3c62f76c0f1d1a75d496
DDI0720 <rlaskdn3615@gmail.com>
2020-06-09 22:35:13 +0900

    어댑터패턴 작업중~

---
+++ typescript/adaper/TurkeyAdapter.ts
@@ -0,0 +1,16 @@
+import {Duck} from './Duck';
+import { Turkey } from './Turkey';
+
+class TurkeyAdapter implements Duck {
+    turkey: Turkey;
+    constructor(turkey: Turkey) {
+        this.turkey = turkey;
+    }
+
+    quack(): void {
+        this.turkey.gooble();
+    }
+    fly(): void {
+        this.turkey.fly();
+    }
+}
